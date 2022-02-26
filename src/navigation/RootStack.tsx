7ec1a1bedc76ef39cc5e90b35ddfeb543fe8015e
Yusuke Matsukuma <yusuke_matsukuma@bita.jp>
2022-02-27 02:07:37 +0900

    chore: install & setting react-navigation

---
+++ src/navigation/RootStack.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Welcome } from '../screens/home/Welcome';
+
+const Stack = createNativeStackNavigator();
+export function RootStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Welcome"
+        component={Welcome}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+}
